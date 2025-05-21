8b0842213e09b982f564ffdb7d2dae09b2c5b869
Firoj <firojahmad07@gmail.com>
2025-05-21 16:57:14 +0530

    update build and port

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,28 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    port: 9002, // Change this to any available port
+    strictPort: true, // Ensures Vite fails if the port is already in use
+    hmr: {
+      host: 'localhost',
+    },
+  },
   build: {
     chunkSizeWarningLimit: 3000,
-  },
+    outDir: '../../public/',
+    emptyOutDir: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'build/admin-bundle.js', // Custom filename for entry files
+        chunkFileNames: 'build/admin-bundle-[hash].js', // Custom filename for chunks
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith('.css')) {
+            return 'build/admin-bundle.css'; // Custom CSS filename
+          }
+          return 'build/admin-bundle-[hash][extname]'; // Default for other assets
+        }
+      }
+    }
+  }
 });
